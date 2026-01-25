3411342956fa625e8bf29b39341e1c78cf4cfd7d
Race Partin <racepartin@gmail.com>
2026-01-24 20:04:04 -0500

    docs(Tile): update Tile.h documentation to XML style

--- Longana/Tile.h
+++ Longana/Tile.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef TILE_H
+#define TILE_H
 
 /**
  * Class Name: Tile
@@ -8,11 +9,16 @@ class Tile {
 public:
     /* --- Constructors --- */
 
-    // Default constructor - set pips to 0
-    // Use member initializer syntax
+    /// <summary>
+    /// Default constructor that initializes both left and right pip counts to 0.
+    /// </summary>
     Tile() : m_left_pips(0), m_right_pips(0) {}
 
-    // Constructor with values
+    /// <summary>
+    /// Constructs a Tile initialized with the given left and right integer values.
+    /// </summary>
+    /// <param name="left">An integer specifying the tile's left value.</param>
+    /// <param name="right">An integer specifying the tile's right value.</param>
     Tile(int left, int right);
 
     /* --- Destructor --- */
@@ -20,32 +26,58 @@ public:
 
     /* --- Selectors --- */
 
-    // Returns the value of the left side
+    /// <summary>
+    /// Returns the stored left pips value (inline const getter).
+    /// </summary>
+    /// <returns>The left pips value as an int.</returns>
     inline int getLeftPips() const { return m_left_pips; }
 
-    // Returns the value of the right side
+    /// <summary>
+    /// Returns the stored right pips value (inline const getter).
+    /// </summary>
+    /// <returns>The right pips value as an int.</returns>
     inline int getRightPips() const { return m_right_pips; }
 
-    // Returns total pips for scoring
+    /// <summary>
+    /// Returns the sum of m_left_pips and m_right_pips.
+    /// </summary>
+    /// <returns>The sum of m_left_pips and m_right_pips.</returns>
     inline int getSum() const { return m_left_pips + m_right_pips; }
 
-    // Check if tiles is a double
+    /// <summary>
+    /// Checks whether the left and right pips are equal.
+    /// </summary>
+    /// <returns>true if m_left_pips equals m_right_pips; otherwise false.</returns>
     inline bool isDouble() const { return m_left_pips == m_right_pips; }
 
-    // Logic for equality
+    /// <summary>
+    /// Compares this Tile with another for equality.
+    /// </summary>
+    /// <param name="other">The Tile to compare against.</param>
+    /// <returns>true if this Tile is equal to other; otherwise false.</returns>
     bool operator==(const Tile& other) const;
 
     /* --- Mutators --- */
 
-    // Set left pips, returns true if successful
+    /// <summary>
+    /// Sets the number of left pips.
+    /// </summary>
+    /// <param name="pips">The number of pips to apply to the left side.</param>
+    /// <returns>true if the left pips were set successfully; otherwise false.</returns>
     bool setLeftPips(int pips);
 
-    // Set right pips, returns true if successful
+    /// <summary>
+    /// Sets the number of pips on the right side.
+    /// </summary>
+    /// <param name="pips">The number of pips to set.</param>
+    /// <returns>true if the pips were set successfully; false otherwise.</returns>
     bool setRightPips(int pips);
 
     /* --- Utility Functions --- */
 
-    // Swap left and right pips
+    /// <summary>
+    /// Flips a tile.
+    /// </summary>
     void flipTile();
 
 private:
@@ -57,3 +89,5 @@ private:
     int m_left_pips;
     int m_right_pips;
 };
+
+#endif
